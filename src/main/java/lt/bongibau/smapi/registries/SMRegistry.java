4c0e62ef1807a52d733aa449bb82f1eb9c33375c
Bongibault Romain <77286154+bongibault-romain@users.noreply.github.com>
2023-03-17 14:38:33 +0100

    fix: make variables private

--- src/main/java/lt/bongibau/smapi/registries/SMRegistry.java
+++ src/main/java/lt/bongibau/smapi/registries/SMRegistry.java
@@ -5,7 +5,7 @@ import lt.bongibau.smapi.registries.exceptions.SMRegistryUnLoadingException;
 
 public abstract class SMRegistry {
 
-    public boolean enabled = false;
+    private boolean enabled = false;
 
     public final void enable() throws SMRegistryLoadingException {
         if (this.isEnabled()) throw new SMRegistryLoadingException("This registry is already enabled.");
